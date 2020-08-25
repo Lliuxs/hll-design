93cda3de8ccd18b1b9054aba1e8ace458bb1cab3
Lliuxs <35122287+Lliuxs@users.noreply.github.com>
2020-08-25 13:48:42 +0000

    Deploying to gh-pages from  @ b28979ff82effc8b23d32d037b7e5f1aed940af2 🚀

---
+++ precache-manifest.8d2c20ad45755de63bcf01c4418a9d62.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b435a7a0275e74ac87d143bb81458c3",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.333ec2bc9e3ca0954cdb.bundle.js"
+  },
+  {
+    "url": "runtime~main.333ec2bc9e3ca0954cdb.bundle.js"
+  },
+  {
+    "url": "vendors~main.333ec2bc9e3ca0954cdb.bundle.js"
+  },
+  {
+    "url": "vendors~main.333ec2bc9e3ca0954cdb.bundle.js.LICENSE.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.8731acfdc52963e37c654e77417e0fc9.js"
+  "precache-manifest.8d2c20ad45755de63bcf01c4418a9d62.js"
 );
 
 self.addEventListener('message', (event) => {
