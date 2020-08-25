b4f4ffd591f515b25f741bc030e5d58cb2984afc
Lliuxs <35122287+Lliuxs@users.noreply.github.com>
2020-08-25 12:30:01 +0000

    Deploying to gh-pages from  @ 6944c77f111de8a09291e07d0a3528a8104bce52 🚀

---
+++ precache-manifest.45faa6b49f88b149bfa2dec7c51dec1a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57ab51c3698e7ea0afec64d619cee755",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.433156162d4088268526.bundle.js"
+  },
+  {
+    "url": "runtime~main.433156162d4088268526.bundle.js"
+  },
+  {
+    "url": "vendors~main.433156162d4088268526.bundle.js"
+  },
+  {
+    "url": "vendors~main.433156162d4088268526.bundle.js.LICENSE.txt"
+  }
+]);
