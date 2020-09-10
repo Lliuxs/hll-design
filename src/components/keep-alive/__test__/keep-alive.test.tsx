57684515ebe1761914efcf50b07aec9f3f9d4deb
liuxs <907478372@qq.com>
2020-09-10 17:54:46 +0800

    keep-alive

---
+++ src/components/keep-alive/__test__/keep-alive.test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { render, fireEvent, cleanup } from "@testing-library/react";
+import { Keep-alive } from "../index";
+import { color, typography } from "../../shared/styles";
+
+
+
+
+describe("test Keep-alive component", () => {
+    it(" ", () => {})
+})
